a0e742efc93270debd07c44e21b70e8db40ae6b0
Chung Nguyen <chung.nguyen@gmail.com>
2017-03-13 22:11:38 -0700

    bomb.js updated

--- bomb.js
+++ bomb.js
@@ -1,3 +1,10 @@
-"use strict";
+// "use strict";
 
 var answers = ["about",	"after", "again",	"below",	"could", "every",	"first",	"found",	"great",	"house", "large",	"learn",	"never",	"other",	"place", "plant",	"point",	"right",	"small", "sound", "spell",	"still",	"study",	"their",	"there", "these",	"thing",	"think",	"three",	"water", "where",	"which",	"world",	"would",	"write"];
+
+
+$('#passwords-1st-letters').keyup(function() {first = $(this).val();});
+$('#passwords-2nd-letters').keyup(function() {second = $(this).val();});
+$('#passwords-3rd-letters').keyup(function() {third = $(this).val();});
+$('#passwords-4th-letters').keyup(function() {fourth = $(this).val();});
+$('#passwords-5th-letters').keyup(function() {fifth = $(this).val();});
