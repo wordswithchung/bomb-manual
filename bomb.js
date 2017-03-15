e9fdccf1b4499434f483912e3c8d9c562e1a3f07
Chung Nguyen <chung.nguyen@gmail.com>
2017-03-14 21:34:21 -0700

    Start loop to display answer

--- bomb.js
+++ bomb.js
@@ -2,9 +2,17 @@
 
 var answers = ["about",	"after", "again",	"below",	"could", "every",	"first",	"found",	"great",	"house", "large",	"learn",	"never",	"other",	"place", "plant",	"point",	"right",	"small", "sound", "spell",	"still",	"study",	"their",	"there", "these",	"thing",	"think",	"three",	"water", "where",	"which",	"world",	"would",	"write"];
 
+var first, second, third, fourth, fifth;
 
 $('#passwords-1st-letters').keyup(function() {first = $(this).val();});
 $('#passwords-2nd-letters').keyup(function() {second = $(this).val();});
 $('#passwords-3rd-letters').keyup(function() {third = $(this).val();});
 $('#passwords-4th-letters').keyup(function() {fourth = $(this).val();});
 $('#passwords-5th-letters').keyup(function() {fifth = $(this).val();});
+
+for (i = 0; i < answers.length; i++) {
+  if (answers[i][0] in first &&
+      answers[i][1] in second) {
+        $('#passwords-answer').text(answers[i]);
+  };
+};
