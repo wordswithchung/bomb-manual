1f6da96d1768480d7d86c72de64a3d747120017a
Chung Nguyen <chung.nguyen@gmail.com>
2017-03-17 23:19:11 -0700

    Add span to test display of answer

--- bomb.js
+++ bomb.js
@@ -9,15 +9,26 @@ var l3 = document.getElementById('pw-letters-3');
 var l4 = document.getElementById('pw-letters-4');
 var l5 = document.getElementById('pw-letters-5');
 
-function addLetters(event) {
+function addLetters() {
   if (l1.value !== null) {letters[0] = l1.value};
   if (l2.value !== null) {letters[1] = l2.value};
   if (l3.value !== null) {letters[2] = l3.value};
   if (l4.value !== null) {letters[3] = l4.value};
   if (l5.value !== null) {letters[4] = l5.value};
   console.log(letters);
+  return displayAnswers();
 }
-//
-// l1.addEventListener("keydown", addLetters(event));
-// l1.addEventListener("keyup", addLetters(event));
-// l1.addEventListener("keypress", addLetters(event));
+
+function displayAnswers() {
+  document.getElementById('passwords-answers').text("here");
+}
+
+// function displayAnswers() {
+//   for (i = 0; i < answers.length; i++) {
+//     console.log(i);
+//     console.log(letters[0]);
+//     if (letters[0].includes(answers[i][0])) {
+//       document.getElementById('passwords-answers').innerHTML(answers[i]);
+//     }
+//   }
+// }
